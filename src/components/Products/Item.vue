b9761c9ec42363a1161b7d054f2b95cc0756907f
Vladimir <remote_racoon@mail.ru>
2019-11-11 13:51:14 +0800

    List of products and product item components created

--- src/components/Products/Item.vue
+++ src/components/Products/Item.vue
@@ -1,10 +1,36 @@
 <template>
-    $END$
+    <div class="mb-4">
+        <div class="card">
+            <div class="card-header">
+                <div class="text-secondary text-capitalize">
+                    {{item.label}}
+                </div>
+            </div>
+            <div class="card-body">
+                <strong class="h4 text-primary mr-3">Rent Rate</strong>
+                {{RentRate}}
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Item"
+        name: "Item",
+        props: {
+            item: {
+                type: Object,
+                required: true
+            }
+        },
+        computed: {
+            /**
+             * @return {string}
+             */
+            RentRate() {
+                return this.item.rent_rate + " $";
+            }
+        }
     }
 </script>
 
--- src/components/Products/List.vue
+++ src/components/Products/List.vue
@@ -1,13 +1,38 @@
 <template>
-    
+    <div>
+        <div class="h3 text-uppercase text-center mt-5">Products List</div>
+        <div class="items-list">
+            <item v-for="item in prods" :item="item" :key="item.id"></item>
+        </div>
+    </div>
 </template>
 
 <script>
+    import Item from './Item';
+    import {mapActions} from 'vuex';
     export default {
-        name: "Products"
+        name: "Products",
+        components: {Item},
+        data() {
+            return {
+                prods: null,
+            }
+        },
+        methods: {
+            ...mapActions('products', [
+                'index'
+            ])
+        },
+        created() {
+            this.index().then(resp => {
+                this.prods = resp;
+            })
+        }
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .items-list {
+        margin-top: 4rem;
+    }
 </style>
