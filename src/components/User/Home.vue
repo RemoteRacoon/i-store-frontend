2917df97cec506feac8db3690dab169d0987bea1
Vladimir <remote_racoon@mail.ru>
2019-11-20 11:06:39 +0800

    User's home component created

--- src/components/User/Home.vue
+++ src/components/User/Home.vue
@@ -1,10 +1,24 @@
 <template>
-    
+    <div class="mt-5">
+        <div class="orders">
+            <orders></orders>
+        </div>
+    </div>
 </template>
 
 <script>
+    import Orders from "../Orders/Orders";
     export default {
-        name: "Home"
+        name: "Home",
+        components: {Orders},
+        created() {
+        },
+        data() {
+            return {
+
+            }
+        },
+
     }
 </script>
 
