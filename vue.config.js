7dd9dab9924433135f62667ecee247cfa6d7e5fd
Vladimir <remote_racoon@mail.ru>
2019-11-20 11:05:59 +0800

    Vue config file modified

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 
 module.exports = {
+    devServer: {
+        overlay: false,
+    },
     outputDir: process.env.NODE_ENV === 'production' ? path.resolve(__dirname, '../../public/dist') : './dist',
 };
