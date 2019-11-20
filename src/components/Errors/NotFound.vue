87e2aebc48aa5fd52580a1b952c9471fe2a566aa
Vladimir <remote_racoon@mail.ru>
2019-11-20 11:03:31 +0800

    Not Found page component added

--- src/components/Errors/NotFound.vue
+++ src/components/Errors/NotFound.vue
@@ -1,5 +1,9 @@
 <template>
-    
+    <div class="container mt-5">
+        <div class="h1 text-center text-dark">
+            Sorry, the page is not found.
+        </div>
+    </div>
 </template>
 
 <script>
