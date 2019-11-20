bc23b9caa56c9eeb1ed474dc8b42cdf0c7149dfc
Vladimir <remote_racoon@mail.ru>
2019-11-20 11:07:03 +0800

    main.js file modified

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import Vue from 'vue'
 import router from './router/router.js';
 import store from './store/store';
 import Master from './layouts/Master';
+import axios from './axios/axios';
 
-require('./styles/styles');
+require('./css/app.scss');
 Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
+
+console.log(Vue.http);
 
 new Vue({
     router,
