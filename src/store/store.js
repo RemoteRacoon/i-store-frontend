bde85ecd05ae0812487d7d0ea613f5b79fd2e251
Vladimir <remote_racoon@mail.ru>
2019-11-20 11:05:30 +0800

    Store created

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,21 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import products from './modules/Products';
+import auth from './modules/Auth';
+import user from './modules/User';
+import orders from './modules/Orders';
+import admin from './modules/Admin';
 
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        products
+        products,
+        auth,
+        user,
+        orders,
+        admin,
     }
 })
 
